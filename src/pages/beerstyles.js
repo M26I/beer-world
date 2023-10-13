cc0a9c50f5ea4bea215a5be0bfcccc5aa99c0352
M26I <marija.ivanisevic267@gmail.com>
2023-10-13 14:06:00 +0100

    banner

--- src/pages/beerstyles.js
+++ src/pages/beerstyles.js
@@ -1,3 +1,18 @@
-import data from "../beer-list.json";
+import Head from "next/head";
+
+export default function BeerStyles () {
+    return(
+        <>
+        <Head>
+        <title>Beer Styles</title>
+        <meta name="description" content="2021. Beer Guide" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/mug.svg" />
+      </Head>
+      <section  className=" h-suto bg-cover bg-center" style={{ backgroundImage: 'url("/bstyles.png")' }}>
+      </section></>
+    )
+
+}
 
 
